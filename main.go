f8e059932a6d5f8d6a2823472538bd7f2ca3d23f
Collin Bleak <loeksnokes@gmail.com>
2021-08-07 23:07:24 +0100

    Added token main.go file.

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import{
+	github.com/loeksnokes/treepair
+	github.com/loeksnokes/prefcode
+}
+
+func main() {
+	tp, _ := NewTreePairAlpha("01")
+	baseCodeToExpand, _ := NewPrefCodeAlphaString("日本語")
+}
